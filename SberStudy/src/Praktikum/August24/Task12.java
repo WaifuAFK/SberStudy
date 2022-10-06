3fff44c62faa9df5f861adf62becb4238090a3be
WaifuAFK <kamextreme147@gmail.com>
2022-10-06 23:50:36 +0300

    Educational Praktikum of August 24

--- SberStudy/src/Praktikum/August24/Task12.java
+++ SberStudy/src/Praktikum/August24/Task12.java
@@ -15,7 +15,24 @@ public class Task12 {
         String a3 = sc.next();
         String a4 = sc.next();
         String a5 = sc.next();
+        String result = "";
 
+        if (a1.charAt(0) < '0' || a1.charAt(0) > '9') {
+            result += a1;
+        }
+        if (a2.charAt(0) < '0' || a2.charAt(0) > '9') {
+            result += a2;
+        }
+        if (a3.charAt(0) < '0' || a3.charAt(0) > '9') {
+            result += a3;
+        }
+        if (a4.charAt(0) < '0' || a4.charAt(0) > '9') {
+            result += a4;
+        }
+        if (a5.charAt(0) < '0' || a5.charAt(0) > '9') {
+            result += a5;
+        }
 
+        System.out.println(result);
     }
 }
