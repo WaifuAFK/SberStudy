38042b31c2b0c19441d2c408983c55232bfbad87
WaifuAFK <kamextreme147@gmail.com>
2022-10-09 18:50:03 +0300

    HW 2 Part 2 was added

--- SberStudy/src/Homework/Number2/Part2/N2Part2Number2.java
+++ SberStudy/src/Homework/Number2/Part2/N2Part2Number2.java
@@ -36,6 +36,7 @@ package Homework.Number2.Part2;
 4. Создать матрицу
  */
 
+
 import java.util.Scanner;
 
 public class N2Part2Number2 {
@@ -43,9 +44,26 @@ public class N2Part2Number2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        int X1 = sc.nextInt();
+        int Y1 = sc.nextInt();
+        int X2 = sc.nextInt();
+        int Y2 = sc.nextInt();
 
         int[][] matrixArray = new int[N][N];
 
+        for (int i = Y1; i <= Y2; i++) {
+            for (int j = X1; j <= X2; j++) {
+                matrixArray[i][j] = 1;
+            }
+        }
+
+
+        for (int[] ints : matrixArray) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
+            }
+            System.out.println();
+        }
         
     }
 }
